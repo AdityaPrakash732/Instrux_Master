190d20396ec5e029f584e98f88779658c36009d0
AdityaPrakash732 <30926043+AdityaPrakash732@users.noreply.github.com>
2025-04-07 05:06:50 +0530

    Init
    tooling project and initial console project added

---
+++ VS_interperter/instrux_console/instrux_console.cpp
@@ -0,0 +1,12 @@
+﻿// instrux_console.cpp : Defines the entry point for the application.
+//
+
+#include "instrux_console.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
