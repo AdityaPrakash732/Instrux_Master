e1041bcba945903de92a112cfa9e96e07693a301
AdityaPrakash732 <30926043+AdityaPrakash732@users.noreply.github.com>
2025-04-09 20:46:07 +0530

    Revert "building up console interface"
    This reverts commit 24751de228dec9e7b359085c47223e7348d8d733.

--- VS_interperter/instrux_console/Console/Headers/Console.h
+++ VS_interperter/instrux_console/Console/Headers/Console.h
@@ -1,24 +1,3 @@
 #pragma once
 
 #include <iostream>
-#include <string>
-
-namespace Instrux
-{
-using value_t = int; // Define value_t as an alias for int (or another appropriate type)
-
-class Console
-{
-public:
-	Console();
-	void StartWin();
-	void StartLinux();
-	void StartMac();
-	value_t GetChar();
-	~Console();
-
-private:
-
-};
-
-}
